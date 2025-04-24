7af2d44a9db74577d9ea0b5ad6709de2d9d6e563
toastmanager <mrbonkyou@gmail.com>
2025-04-24 19:29:43 +0900

    feat: status for ticket

--- src/lib/types/tickets/ticket.ts
+++ src/lib/types/tickets/ticket.ts
@@ -1,6 +1,12 @@
 import { TicketType } from "./ticket-type";
 import { User } from "../auth/user";
 
+export type TicketStatus =
+  | "PENDING_REVIEW"
+  | "IN_PROGRESS"
+  | "COMPLETED"
+  | "REJECTED";
+
 export type Ticket = {
   id: number;
   title: string;
@@ -10,4 +16,5 @@ export type Ticket = {
   longitude: number;
   user: User;
   type: TicketType;
+  status: TicketStatus;
 };
